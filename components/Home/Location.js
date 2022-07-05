b558b7a48e1210a822d13dc54acc0ddddd64ea90
khanfari <105904156+khanfari@users.noreply.github.com>
2022-07-05 17:59:50 +0530

    sections in main page - demo deploy

---
+++ components/Home/Location.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import Link from 'next/link'
+function Location() {
+  return (
+    <section className="location section-gap light-grey-bg" id="cynic-contact" data-scroll-offset="75"
+    data-scroll-offset-mobile="75">
+    <div className="container">
+        <h2 className="section-title text-center">Our Location</h2>
+   
+
+
+        <div className="contact-info">
+            <div className="row justify-content-between">
+                <div className="col-lg-5 ">
+                    <div className="info">
+                        <h5>DigitalQ UK IT Solutions Ltd</h5>
+                        <p>Orega Slough,<br/>The Switch 1-7,<br/>The Grove, Slough - SL1 1PQ.</p>
+                    </div>
+                </div>
+                <div className="col-lg-3 ">
+                    <div className="info">
+                        <h5>Email</h5>
+                        <Link href="mailto:hello@digitalqits.com"><a>hello@digitalqits.com</a></Link>
+                    </div>
+                </div>
+                <div className="col-lg-4 ">
+                    <div className="info">
+                        <h5>Phone</h5>
+                        <Link href="tel:+44175339920"><a>+44 175-339-920</a></Link>
+                       
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+
+  <div className="contact-info">
+            <div className="row justify-content-between">
+                <div className="col-lg-5 ">
+                    <div className="info">
+                        <h5>India</h5>
+                        <p>#23, Cholan Street, <br/>Myilarasu Nagar,<br/>Chrompet, Chennai - 600 044.</p>
+                    </div>
+                </div>
+                <div className="col-lg-3 ">
+                    <div className="info">
+                        <h5>Email</h5>
+                        <Link href="mailto:hello@digitalq.co.in"><a>hello@digitalq.co.in</a></Link>
+                    </div>
+                </div>
+                <div className="col-lg-4 ">
+                    <div className="info">
+                        <h5>Phone</h5>
+                                                    <Link href="tel:+914422653315"><a>+91 (044) 2265-1990</a></Link>
+                        <Link href="tel:+914422653315"><a>+91 (044) 2265-3315</a></Link>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+
+        <div className="contact-info">
+            <div className="row justify-content-between">
+                <div className="col-lg-5 ">
+                    <div className="info">
+                        <h5>USA</h5>
+                        <p>9 Rockingham Ct, Germantown,<br/> MD, 20874, Montgomery Country,<br/> Maryland</p>
+                    </div>
+                </div>
+                <div className="col-lg-3 ">
+                    <div className="info">
+                        <h5>Email</h5>
+                        <Link href="mailto:cloudcas@digitalq.net"><a>cloudcas@digitalq.net</a></Link>
+                    </div>
+                </div>
+                <div className="col-lg-4 ">
+                    <div className="info">
+                        <h5>Phone</h5>
+                        <Link href="tel:+914422653315"><a>(+202) 629 7029</a></Link>
+                       
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+        <div className="contact-info">
+            <div className="row justify-content-between">
+                <div className="col-lg-5 ">
+                    <div className="info">
+                        <h5>Canada</h5>
+                        <p>960 Markham Road, Scarborough,<br/>Ontario - M1H2Y4</p>
+                    </div>
+                </div>
+                <div className="col-lg-3 ">
+                    <div className="info">
+                        <h5>Email</h5>
+                        <Link href="mailto:welcome@digitalqglobal.ca"><a>welcome@digitalqglobal.ca</a></Link>
+                    </div>
+                </div>
+                <div className="col-lg-4 ">
+                    <div className="info">
+                        <h5>Phone</h5>
+                        <Link href="tel:+914422653315"><a>+1 647 819 7401</a></Link>
+                       
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+  )
+}
+
+export default Location
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Footer from '../components/Home/Footer'
 import Header from '../components/Home/Header'
 import Banner from '../components/Home/Banner'
 
-// import Client from '../components/Home/Client'
-// import Features from '../components/Home/Features'
-// import Location from '../components/Home/Location'
+import Client from '../components/Home/Client'
+import Features from '../components/Home/Features'
+import Location from '../components/Home/Location'
 // import Section from '../components/Home/Section'
 
 export default function Home() {
@@ -13,10 +13,10 @@ export default function Home() {
     <div>
       <Header/>
       <Banner/>
-      {/* <Section/>
-      <Features/>
+      {/* <Section/> */}
+      <Features/> 
       <Client/>
-      <Location/> */}
+      <Location/> 
       <Footer/>
     </div>
   )
